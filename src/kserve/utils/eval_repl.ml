e67d7c32b79fb7d48d43b05dc1650994542311ae
ekatsym <ekatsymeee@gmail.com>
2021-04-19 12:06:11 +0900

    Added REPL of eval.

--- src/kserve/utils/eval_repl.ml
+++ src/kserve/utils/eval_repl.ml
@@ -1,1 +1,20 @@
-let () = print_endline "hello"
+let () =
+  let open Format in
+  let open Genslib.Intf in
+  let open Gensl_eval in
+  let open Eval in
+  let rec loop () =
+    printf "repl> "; print_flush ();
+    let line = read_line () in
+    match line with
+    | "(exit)" -> ()
+    | _ ->
+      (try Printexc.print
+             (fun l -> ctree_of_string l
+                       |> eval_ctree
+                       |> CanonicaltreeFlavor.to_string
+                       |> print_endline)
+             line
+       with _ -> printf "got some error. try again pls.\n");
+      print_flush (); loop (); in
+  loop ()
